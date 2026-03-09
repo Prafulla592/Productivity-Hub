fd49d6e6c453fbd6b33b1b0cc6be7e5e3b53b6a1
JawaidR <rashid.jawaid@patronus.ai>
2026-03-09 15:13:11 +0530

    fix port for render deployment

--- server/index.ts
+++ server/index.ts
@@ -89,15 +89,22 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || "5000", 10);
-  httpServer.listen(
-    {
-      port,
-      host: "localhost",
-      reusePort: true,
-    },
-    () => {
-      log(`serving on port ${port}`);
-    },
-  );
+  // const port = parseInt(process.env.PORT || "5000", 10);
+  // httpServer.listen(
+  //   {
+  //     port,
+  //     host: "localhost",
+  //     reusePort: true,
+  //   },
+  //   () => {
+  //     log(`serving on port ${port}`);
+  //   },
+  // );
+
+
+  const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 })();
